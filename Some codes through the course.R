d167fac854c87f860aa3f8d827d838ff67ded610
hectorla03 <hectorla03@gmail.com>
2021-12-28 11:20:35 -0400

    adding a script for random codes

---
+++ Some codes through the course.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(dslabs)
+data(gapminder)
+
+# create and inspect a tidy data frame
+tidy_data <- gapminder %>% 
+  filter(country %in% c("South Korea", "Germany")) %>%
+  select(country, year, fertility)
+head(tidy_data)
+
+# plotting tidy data is simple
+tidy_data %>% 
+  ggplot(aes(year, fertility, color = country)) +
+  geom_point()
