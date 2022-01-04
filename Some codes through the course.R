d9735e0991a88531dd5eace68b63e0aa278442ef
hectorla03 <hectorla03@gmail.com>
2022-01-04 15:35:07 -0400

    Update 01/04/2022

--- Some codes through the course.R
+++ Some codes through the course.R
@@ -12,3 +12,202 @@ head(tidy_data)
 tidy_data %>% 
   ggplot(aes(year, fertility, color = country)) +
   geom_point()
+ 
+#Chapter 21 Reshaping data
+
+# Key points
+The tidyr package includes several functions that are useful for tidying data.
+The gather() function converts wide data into tidy data.
+The spread() function converts tidy data to wide data.
+
+# original wide data
+library(tidyverse) 
+path <- system.file("extdata", package="dslabs")
+filename <- file.path(path,  "fertility-two-countries-example.csv")
+wide_data <- read_csv(filename)
+
+# tidy data from dslabs
+library(dslabs)
+data("gapminder")
+tidy_data <- gapminder %>% 
+  filter(country %in% c("South Korea", "Germany")) %>%
+  select(country, year, fertility)
+
+# gather wide data to make new tidy data
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, `1960`:`2015`)
+head(new_tidy_data)
+
+# gather all columns except country
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, -country)
+
+# gather treats column names as characters by default
+class(tidy_data$year)
+class(new_tidy_data$year)
+
+# convert gathered column names to numeric
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, -country, convert = TRUE)
+class(new_tidy_data$year)
+
+# ggplot works on new tidy data
+new_tidy_data %>%
+  ggplot(aes(year, fertility, color = country)) +
+  geom_point()
+
+# spread tidy data to generate wide data
+new_wide_data <- new_tidy_data %>% spread(year, fertility)
+select(new_wide_data, country, `1960`:`1967`)
+
+################# Codes from Funtion Separate and Unite
+
+# import data
+path <- system.file("extdata", package = "dslabs")
+filename <- file.path(path, "life-expectancy-and-fertility-two-countries-example.csv")
+raw_dat <- read_csv(filename)
+select(raw_dat, 1:5)
+
+# gather all columns except country
+dat <- raw_dat %>% gather(key, value, -country)
+head(dat)
+dat$key[1:5]
+
+# separate on underscores
+dat %>% separate(key, c("year", "variable_name"), "_") #default separate argument is "_"
+dat %>% separate(key, c("year", "variable_name"))
+
+# split on all underscores, pad empty cells with NA
+dat %>% separate(key, c("year", "first_variable_name", "second_variable_name"), 
+                 fill = "right")
+
+# split on first underscore but keep life_expectancy merged
+dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge")
+
+# separate then spread
+dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge") %>%
+  spread(variable_name, value) 
+
+# separate then unite
+dat %>% 
+  separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
+  unite(variable_name, first_variable_name, second_variable_name, sep="_")
+
+# full code for tidying data
+dat %>% 
+  separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
+  unite(variable_name, first_variable_name, second_variable_name, sep="_") %>%
+  spread(variable_name, value) %>%
+  rename(fertility = fertility_NA)
+
+################# Codes Chapter 22 Joining tables
+
+Key points
+The join functions in the dplyr package combine two tables such that matching rows are together.
+left_join() only keeps rows that have information in the first table.
+right_join() only keeps rows that have information in the second table.
+inner_join() only keeps rows that have information in both tables.
+full_join() keeps all rows from both tables.
+semi_join() keeps the part of first table for which we have information in the second.
+anti_join() keeps the elements of the first table for which there is no information in the second.
+
+# import US murders data
+library(tidyverse)
+library(ggrepel)
+library(dslabs)
+ds_theme_set()
+data(murders)
+head(murders)
+
+# import US election results data
+data(polls_us_election_2016)
+head(results_us_election_2016)
+identical(results_us_election_2016$state, murders$state)
+
+# join the murders table and US election results table
+tab <- left_join(murders, results_us_election_2016, by = "state")
+head(tab)
+
+# plot electoral votes versus population
+tab %>% ggplot(aes(population/10^6, electoral_votes, label = abb)) +
+  geom_point() +
+  geom_text_repel() + 
+  scale_x_continuous(trans = "log2") +
+  scale_y_continuous(trans = "log2") +
+  geom_smooth(method = "lm", se = FALSE)
+
+# make two smaller tables to demonstrate joins
+tab1 <- slice(murders, 1:6) %>% select(state, population)
+tab1
+tab2 <- slice(results_us_election_2016, c(1:3, 5, 7:8)) %>% select(state, electoral_votes)
+tab2
+
+# experiment with different joins
+left_join(tab1, tab2)
+tab1 %>% left_join(tab2)
+tab1 %>% right_join(tab2)
+inner_join(tab1, tab2)
+semi_join(tab1, tab2)
+anti_join(tab1, tab2)
+
+### Set Operators
+
+# intersect vectors or data frames
+intersect(1:10, 6:15)
+intersect(c("a","b","c"), c("b","c","d"))
+tab1 <- tab[1:5,]
+tab2 <- tab[3:7,]
+intersect(tab1, tab2)
+
+# perform a union of vectors or data frames
+union(1:10, 6:15)
+union(c("a","b","c"), c("b","c","d"))
+tab1 <- tab[1:5,]
+tab2 <- tab[3:7,]
+union(tab1, tab2)
+
+# set difference of vectors or data frames
+setdiff(1:10, 6:15)
+setdiff(6:15, 1:10)
+tab1 <- tab[1:5,]
+tab2 <- tab[3:7,]
+setdiff(tab1, tab2)
+
+# setequal determines whether sets have the same elements, regardless of order
+setequal(1:5, 1:6)
+setequal(1:5, 5:1)
+setequal(tab1, tab2)
+
+################# Codes Chapter 23 Web Scraping
+
+Key points
+Web scraping is extracting data from a website.
+The rvest web harvesting package includes functions to extract nodes of an HTML document: html_nodes() extracts all nodes of different types, and html_node() extracts the first node.
+html_table() converts an HTML table to a data frame.
+
+# import a webpage into R
+library(rvest)
+url <- "https://en.wikipedia.org/wiki/Gun_violence_in_the_United_States_by_state"
+h <- read_html(url)
+class(h)
+h
+
+tab <- h %>% html_nodes("table")
+tab <- tab[[2]]
+
+tab <- tab %>% html_table
+class(tab)
+
+tab <- tab %>% setNames(c("state", "population", "total", "murders", "gun_murders", "gun_ownership", "total_rate", "murder_rate", "gun_murder_rate"))
+head(tab)
+
+#Another example from web scraping using google extention (SelectorGadget)
+
+h <- read_html("http://www.foodnetwork.com/recipes/alton-brown/guacamole-recipe-1940609")
+recipe <- h %>% html_node(".o-AssetTitle__a-HeadlineText") %>% html_text()
+prep_time <- h %>% html_node(".m-RecipeInfo__a-Description--Total") %>% html_text()
+ingredients <- h %>% html_nodes(".o-Ingredients__a-Ingredient") %>% html_text()
+
+guacamole <- list(recipe, prep_time, ingredients)
+
+
